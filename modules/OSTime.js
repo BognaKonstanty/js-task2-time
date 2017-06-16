2457cd9a3a584f7342616628a6d80a4a4a054d64
Bogna KOnstanty <rosiakb@gmail.com>
2017-06-16 10:30:19 +0200

    simplify function of time

--- modules/OSTime.js
+++ modules/OSTime.js
@@ -16,21 +16,28 @@ process.stdin.setEncoding('utf-8');
 });*/
 
 function convertOSTime(seconds) {
-            var hours;
+            /*var hours;
             var minutes;
             var seconds;
           if (seconds < 3600) {
                hours= Math.floor(seconds/3600);
               minutes = Math.floor(seconds/60);
               seconds = seconds%60;
-            return [hours + " hours", minutes + " minutes", seconds + " seconds"];
+            return hours + " hours, " + minutes + " minutes, " + seconds + " seconds";
         }
            else {
               hours= Math.floor(seconds/3600);
               minutes = Math.floor(seconds/60)%60;
               seconds = seconds%60;
             return hours + " hours, " + minutes + " minutes, " + seconds + " seconds";
-        }
+          }*/
+
+            var hours   = Math.floor(seconds / 3600);
+            var minutes = Math.floor((seconds - (hours * 3600)) / 60);
+            var seconds = seconds - (hours * 3600) - (minutes * 60);
+            
+            return hours + " hours, " + minutes + " minutes, " + seconds + " seconds";
+        
         //console.log(convertOStime(125));
 }
 //}
